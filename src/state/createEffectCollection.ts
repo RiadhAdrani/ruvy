a5ea630b1ddfb2af48ec5474f69d0d8f30038873
RiadhAdrani <riadh_adrani@hotmail.fr>
2023-02-15 13:44:54 +0100

    feat(Store): add store api

---
+++ src/state/createEffectCollection.ts
@@ -0,0 +1,64 @@
+import { areEqual } from "@riadh-adrani/utility-js";
+import { StoreEffect, StoreEffectsCollection } from "../types";
+import Store from "./Store";
+
+type Options = {
+  name: string;
+  keepUnused: boolean;
+};
+
+export default (manager: Store, { name, keepUnused }: Options): StoreEffectsCollection => {
+  return {
+    items: {},
+    name,
+    used: [],
+    set: (key, effect, dep) => {
+      const item: StoreEffect | undefined = manager.effects[name].items[key];
+
+      manager.effects[name].used.push(key);
+
+      if (item) {
+        const current = item.dep;
+
+        if (!areEqual(current, dep)) {
+          item.shouldRun = true;
+          item.effect = effect;
+          item.dep = dep;
+        }
+      } else {
+        const item: StoreEffect = {
+          called: 0,
+          dep,
+          effect,
+          key,
+          shouldRun: true,
+        };
+
+        manager.effects[name].items[key] = item;
+      }
+    },
+    clean: (item) => {
+      if (keepUnused) {
+        return;
+      }
+
+      delete manager.effects[name].items[item.key];
+    },
+    runner: (item) => {
+      item.cleanUp?.();
+
+      if (!item.shouldRun) {
+        return;
+      }
+
+      const cleanUp = item.effect();
+
+      if (cleanUp) {
+        item.cleanUp = cleanUp;
+      }
+
+      item.called = item.called + 1;
+      item.shouldRun = false;
+    },
+  };
+};
