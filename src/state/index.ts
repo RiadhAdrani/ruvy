bda256c4947cb2157a1d8f1bc36416146b37003a
RiadhAdrani <riadh_adrani@hotmail.fr>
2023-02-15 13:57:25 +0100

    chore(State): add missing exports

--- src/state/index.ts
+++ src/state/index.ts
@@ -1,1 +1,3 @@
-export {};
+export { default as Store } from "./Store";
+export { default as createEffectCollection } from "./createEffectCollection";
+export { default as createStateCollection } from "./createStateCollection";
