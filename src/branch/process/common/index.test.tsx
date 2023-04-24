3642bf9141996ba14b0bda894a6af2e1d533d536
RiadhAdrani <riadh_adrani@hotmail.fr>
2023-04-24 18:58:45 +0100

    test: unmountBranch

--- src/branch/process/common/index.test.tsx
+++ src/branch/process/common/index.test.tsx
@@ -5,9 +5,10 @@
 import { createJsxElement, createFragmentTemplate } from "../../create/index.js";
 import { beforeEach, describe, expect, it, vitest } from "vitest";
 import root from "../new/root.js";
-import { collectActions, commit } from "./index.js";
+import { collectActions, commit, unmountBranch } from "./index.js";
 import { setState } from "../../hooks/index.js";
-import { Branch } from "../../types/index.js";
+import { ActionType, Branch, BranchStatus, BranchTag } from "../../types/index.js";
+import { initBranch } from "../../utils/index.js";
 
 describe("common", () => {
   beforeEach(() => {
@@ -19,6 +20,56 @@ describe("common", () => {
 
     branch.children.forEach(expectPendingActionsToBeEmpty);
   };
+
+  describe("unmountBranch", () => {
+    let branch: Branch;
+
+    beforeEach(() => {
+      branch = initBranch();
+    });
+
+    it("should change status to unmounting", () => {
+      unmountBranch(branch);
+
+      expect(branch.status).toBe(BranchStatus.Unmounting);
+    });
+
+    it.each([[BranchTag.Element], [BranchTag.Text]])(
+      "should add an Unmount action to pendingActions if (%s)",
+      (tag) => {
+        branch = initBranch({ tag });
+
+        unmountBranch(branch);
+
+        expect(branch.pendingActions.some((a) => a.type === ActionType.Unmount)).toBe(true);
+      }
+    );
+
+    it.each([[BranchTag.Fragment], [BranchTag.Function], [BranchTag.Null]])(
+      "should not add an Unmount action to pendingActions if (%s)",
+      (tag) => {
+        branch = initBranch({ tag });
+
+        unmountBranch(branch);
+
+        expect(branch.pendingActions.some((a) => a.type === ActionType.Unmount)).toBe(false);
+      }
+    );
+
+    it("should unmount children recursively", () => {
+      branch = initBranch();
+
+      branch.children.push(...[initBranch(), initBranch()]);
+
+      unmountBranch(branch);
+
+      const checkRecursively = (target: Branch) => {
+        expect(target.pendingActions.some((a) => a.type === ActionType.Unmount));
+
+        target.children.forEach(checkRecursively);
+      };
+    });
+  });
 
   describe("commit", () => {
     it("should render tree into the dom", () => {
