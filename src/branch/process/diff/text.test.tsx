de49b1caa80faf11a48933ac229a9149b40ffbe3
RiadhAdrani <riadh_adrani@hotmail.fr>
2023-04-27 18:50:22 +0100

    test: diff.text

---
+++ src/branch/process/diff/text.test.tsx
@@ -0,0 +1,36 @@
+/** @jsx createJsxElement */
+/** @jsxFrag createFragmentTemplate */
+
+// @ts-ignore
+import { createFragmentTemplate, createJsxElement } from "../../create/index.js";
+import { describe, expect, it } from "vitest";
+import { initBranch } from "../../utils/index.js";
+import { ActionType, BranchTag } from "../../types/index.js";
+import text from "./text.js";
+
+describe("diff.text", () => {
+  it("should return empty array", () => {
+    const branch = initBranch<string>({ type: BranchTag.Text, text: "hello" });
+
+    const res = text(branch, "");
+
+    expect(res).toStrictEqual([]);
+  });
+
+  it("should update text value", () => {
+    const branch = initBranch<string>({ type: BranchTag.Text, text: "hello" });
+
+    text(branch, "world");
+
+    expect(branch.text).toStrictEqual("world");
+  });
+
+  it("should create a text update action", () => {
+    const branch = initBranch<string>({ type: BranchTag.Text, text: "hello" });
+
+    text(branch, "world");
+
+    expect(branch.pendingActions.length).toStrictEqual(1);
+    expect(branch.pendingActions[0].type).toStrictEqual(ActionType.UpdateText);
+  });
+});
