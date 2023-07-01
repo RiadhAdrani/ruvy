02745329641ce8de9700040bebb900f8e745ee8a
RiadhAdrani <riadh_adrani@hotmail.fr>
2023-07-01 13:44:13 +0100

    feat: add handleOutletComponent

--- src/branch/components/outlet/outlet.test.tsx
+++ src/branch/components/outlet/outlet.test.tsx
@@ -5,7 +5,7 @@ import { createJsxElement } from '../../create/index.js';
 import { Core, createRouter } from '../../../core/index.js';
 import { Outlet } from '../../index.js';
 import { initBranch } from '../../utils/index.js';
-import outlet from './outlet.js';
+import outlet, { handleOutletComponent } from './outlet.js';
 import { BranchTag } from '../../types.js';
 
 createJsxElement;
@@ -40,5 +40,40 @@ describe('new.outlet', () => {
     expect(branch.children.length).toBe(1);
     expect(branch.children[0].tag).toBe(BranchTag.Text);
     expect(branch.children[0].text).toBe('root element');
+  });
+});
+
+describe('handleOutletComponent', () => {
+  beforeEach(() => {
+    new Core();
+
+    createRouter([{ path: '/', component: 'root element' }], {});
+  });
+
+  it('should create an outlet branch', () => {
+    new Core();
+
+    const parent = initBranch();
+    const { branch } = handleOutletComponent(<Outlet />, undefined, parent, 0);
+
+    expect(branch.type).toStrictEqual(Outlet);
+    expect(branch.tag).toStrictEqual(BranchTag.Outlet);
+  });
+
+  it('should return [undefined] as children', () => {
+    new Core();
+
+    const parent = initBranch();
+    const { unprocessedChildren } = handleOutletComponent(<Outlet />, undefined, parent, 0);
+
+    expect(unprocessedChildren).toStrictEqual([undefined]);
+  });
+
+  it('should return route component', () => {
+    const parent = initBranch();
+
+    const { unprocessedChildren } = handleOutletComponent(<Outlet />, undefined, parent, 0);
+
+    expect(unprocessedChildren).toStrictEqual(['root element']);
   });
 });
