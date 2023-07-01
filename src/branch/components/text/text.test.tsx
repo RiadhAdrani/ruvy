fef693af06be40e4f690d1274ac8c95ff1d453e4
RiadhAdrani <riadh_adrani@hotmail.fr>
2023-07-01 14:12:53 +0100

    feat: add createTextComponent

--- src/branch/components/text/text.test.tsx
+++ src/branch/components/text/text.test.tsx
@@ -4,7 +4,7 @@ import { createJsxElement } from '../../create/index.js';
 import { describe, expect, it } from 'vitest';
 import { initBranch } from '../../utils/index.js';
 import { ActionType, BranchStatus, BranchTag } from '../../types.js';
-import text from './text.js';
+import text, { handleTextComponent } from './text.js';
 import { omit } from '@riadh-adrani/utils';
 
 createJsxElement;
@@ -56,5 +56,40 @@ describe('diff.text', () => {
 
     expect(branch.pendingActions.length).toStrictEqual(1);
     expect(branch.pendingActions[0].type).toStrictEqual(ActionType.UpdateText);
+  });
+});
+
+describe('handleTextComponent', () => {
+  const parent = initBranch();
+
+  it('should create a text branch', () => {
+    const { branch, unprocessedChildren } = handleTextComponent('test', undefined, parent, 0);
+
+    expect(omit(branch, 'pendingActions')).toStrictEqual({
+      children: [],
+      hooks: {},
+      key: 0,
+      props: {},
+      status: BranchStatus.Mounting,
+      tag: BranchTag.Text,
+      type: BranchTag.Text,
+      parent,
+      text: 'test',
+      unmountedChildren: [],
+    });
+
+    expect(branch.pendingActions.length).toBe(1);
+    expect(branch.pendingActions[0].type).toBe(ActionType.Render);
+
+    expect(unprocessedChildren).toStrictEqual([]);
+  });
+
+  it('should update text value and create an action', () => {
+    const branch = initBranch<string>({ type: BranchTag.Text, text: 'hello' });
+
+    handleTextComponent('world', branch, parent, 0);
+
+    expect(branch.text).toStrictEqual('world');
+    expect(branch.pendingActions.some(it => it.type === ActionType.UpdateText)).toBe(true);
   });
 });
--- src/branch/components/text/text.ts
+++ src/branch/components/text/text.ts
@@ -4,10 +4,12 @@ import {
   BranchKey,
   BranchStatus,
   BranchTag,
+  ComponentFunctionHandler,
   ComponentHandler,
 } from '../../types.js';
 import createAction from '../../actions/index.js';
 import { areEqual } from '@riadh-adrani/utils';
+import { initBranch } from '../../utils/index.js';
 
 /**
  * create new text branch from data.
@@ -55,6 +57,36 @@ const diff = (template: string, current: Branch<string>): Array<unknown> => {
 const textComponentHandler: ComponentHandler<string, string> = {
   create,
   diff,
+};
+
+export const handleTextComponent: ComponentFunctionHandler<string, string> = (
+  template,
+  current,
+  parent,
+  key
+) => {
+  const branch: Branch<string> =
+    current ??
+    initBranch({
+      key,
+      tag: BranchTag.Text,
+      type: BranchTag.Text,
+      text: template,
+      parent,
+    });
+
+  if (!current) {
+    branch.pendingActions.push(createAction(ActionType.Render, branch));
+  } else {
+    // we check if text are different
+    if (!areEqual(current.text, template)) {
+      current.pendingActions.push(createAction(ActionType.UpdateText, current, template));
+
+      current.text = template;
+    }
+  }
+
+  return { branch, unprocessedChildren: [] };
 };
 
 export default textComponentHandler;
