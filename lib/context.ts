9561181dc3ea76ee5cf3736abd6a92d3c61a61e6
RiadhAdrani <riadh_adrani@hotmail.fr>
2023-12-12 00:05:48 +0100

    chore: remake types

---
+++ lib/context.ts
@@ -0,0 +1,37 @@
+import { Context, ContextType } from '@/types.js';
+import { RuvyError } from './helpers.js';
+
+const stack: Array<Context> = [];
+
+let ctx: Context = { type: ContextType.Idle };
+
+export const getContext = (): Context => ctx;
+
+export const getContextOrThrow = <T extends Context>(type: ContextType): T => {
+  if (ctx.type !== type) throw new RuvyError('requested state does not match the current one');
+
+  return ctx as T;
+};
+
+export const runInContext = <V extends Context, R = void>(
+  value: V,
+  callback: () => R,
+  onEnd?: () => void
+): R => {
+  stack.push(ctx);
+  ctx = value;
+
+  const result = callback();
+
+  const v = stack.pop();
+
+  if (!v) {
+    throw new RuvyError('unexpected state: no context found in stack');
+  }
+
+  ctx = v;
+
+  onEnd?.();
+
+  return result;
+};
