24466c21dbec01cf83f495916b0f4985208a3e94
RiadhAdrani <riadh_adrani@hotmail.fr>
2024-02-04 11:17:49 +0100

    chore(deps): bump @riadh-adrani/dom-router version

--- docs/types/index.ts
+++ docs/types/index.ts
@@ -6,7 +6,7 @@ export enum Theme {
   Device = 'device',
 }
 
-export const Versions = ['0.5.0', '0.5.2', '0.5.3', '0.5.4'] as const;
+export const Versions = ['0.5.0', '0.5.2', '0.5.3', '0.5.4', '0.5.5'] as const;
 
 export type Version = (typeof Versions)[number];
 
