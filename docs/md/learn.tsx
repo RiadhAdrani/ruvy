df1523e065e17e130f998ca490dc7c0553795466
RiadhAdrani <riadh_adrani@hotmail.fr>
2024-02-21 17:28:57 +0100

    chore(docs): learn's setup doc

--- docs/md/learn.tsx
+++ docs/md/learn.tsx
@@ -1,9 +1,9 @@
 import { DocItem } from '../types/index.js';
 
-import setup from './learn/setup.md?raw';
 import tutorialTodo from './learn/tutorial-todo.md?raw';
 import index__versions from './learn/index/md.js';
 import quickStart__versions from './learn/quick-start/md.js';
+import setup__versions from './learn/setup/md.js';
 
 export const LearnSections: Array<DocItem> = [
   { path: '', versions: index__versions },
@@ -14,12 +14,7 @@ export const LearnSections: Array<DocItem> = [
   },
   {
     path: '/setup',
-    versions: [
-      {
-        from: '0.5.0',
-        md: setup,
-      },
-    ],
+    versions: setup__versions,
     title: 'Setup',
   },
   {
